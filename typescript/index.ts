0252daa71569597ff8163286c8b34f6e6be2b326
archibald1418 <liam141810@gmail.com>
2023-01-31 20:24:18 +0000

    implicit vs explicit types

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,4 +1,7 @@
-// import * as _ from 'lodash' 
+import * as _ from 'lodash'
+    // lodash library has community supported
+    // npm i -D @types/lodash - loads types for certain libraries
+    // npm has a monorepo @types with community supported types
 
 console.log('hello world')
 // Typescript compiler 'tsc' transpiles into vanilla JS
@@ -12,3 +15,24 @@ async function hello(){
 
 
 // tsc ES versions etc. can be configured via tsconfig.json
+
+
+// .................. type annotations
+
+// implicit typing
+let lucky = 23; // type will be inferred
+lucky = '23'; // while vanilla don't mind, tsc will find a type error
+
+let opt_out: any = 23; // 'any' annotation lets you "opt out" from type checking
+opt_out = '23' // resulting in no errors
+
+let untyped; // inferred as any
+untyped = '23'
+untyped = 23
+
+let num_typed: number; // explicit typing
+num_typed = 23
+num_typed = '23' // checks the types resulting in error
+
+// if you have implicit type, don't bother explicitly typing it
+// let num = 23 is just fine
