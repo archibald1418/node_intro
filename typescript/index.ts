e7828d724f4eb223e7801ac779d0daebb558d2f4
archibald1418 <liam141810@gmail.com>
2023-01-31 19:59:52 +0000

    typescript started

---
+++ typescript/index.ts
@@ -0,0 +1,12 @@
+console.log('hello world')
+// Typescript compiler 'tsc' transpiles into vanilla JS
+
+
+async function hello(){
+    return 'world'
+}
+// by default tsc is EcmaScript 3..., doesn't have support for async await
+// will add extra crazy code to replicate async 
+
+
+// tsc ES versions etc. can be configured via tsconfig.json
